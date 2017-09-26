815a79b30b547a219955db178e45fb1ef7e7b4f7
lifengshuo <fengshuo_li@163.com>
2017-09-26 10:40:16 +0800

    union find two ways

--- Union-Find.java
+++ Union-Find.java
@@ -3,46 +3,55 @@ import java.util.Arrays;
 /**
  * Created by shuoshuo on 2017/9/25.
  */
-public class Union-Find {
+public class UnionFind {
 	public static void main(String[] args) {
 		int v = 3, e = 3;
 		Graph graph = new Graph(v, e);
 
-		graph.edge[0].src = 0;
-		graph.edge[0].dest = 1;
+		graph.edges[0].src = 0;
+		graph.edges[0].dest = 1;
 
-		graph.edge[1].src = 1;
-		graph.edge[1].dest = 2;
+		graph.edges[1].src = 1;
+		graph.edges[1].dest = 2;
 
-		graph.edge[2].src = 0;
-		graph.edge[2].dest = 2;
+		graph.edges[2].src = 0;
+		graph.edges[2].dest = 2;
 
-		if (graph.isCycle(graph) == 1) {
+		if (graph.isCircle2(graph)) {
 			System.out.println("hahah");
+		}
+
+		Graph friends = new Graph(5, 5);
+		friends.union2(friends.rank, friends.parent, 0, 2);
+		friends.union2(friends.rank, friends.parent, 4, 2);
+		friends.union2(friends.rank, friends.parent, 3, 1);
+
+		if (friends.find2(friends.parent, 0) == friends.find2(friends.parent, 4)) {
+			System.out.println("yes");
 		}
 	}
 }
 
 class Graph {
-	int v, e; //count of vertices and edges
-	Edge edge[];
+	int v, e;
+	Edge[] edges;
+	int[] parent;
+	int[] rank;
 
-	//边的起点与终点
-	class Edge {
-		int src, dest;
-	}
-
-	Graph(int v, int e) {
+	Graph (int v, int e) {
 		this.v = v;
 		this.e = e;
-		edge = new Edge[e];
+		edges = new Edge[v];
+		parent = new int[v];
+		rank = new int[v];
+
 		for (int i=0; i<v; i++) {
-			edge[i] = new Edge();
+			edges[i] = new Edge();
 		}
+		Arrays.fill(parent, -1);
 	}
 
-	//find parent node
-	int find(int parent[], int i) {
+	int find (int[] parent, int i) {
 		if (parent[i] == -1) {
 			return i;
 		}
@@ -50,29 +59,75 @@ class Graph {
 		return find(parent, parent[i]);
 	}
 
-	//join two sets
-	void union(int parent[], int x, int y) {
+	void union(int[] parent, int x, int y) {
 		int xset = find(parent, x);
 		int yset = find(parent, y);
 
 		parent[xset] = yset;
 	}
 
-	int isCycle(Graph graph) {
-		int[] parent = new int[graph.v];
-		Arrays.fill(parent, -1);
-
-		for (int i=0; i<graph.v; i++) {
-			int x = graph.find(parent, graph.edge[i].src);
-			int y = graph.find(parent, graph.edge[i].dest);
+	boolean isCircle(Graph graph) {
+		for (int i=0; i<v; i++) {
+			int x = find(graph.parent, graph.edges[i].src);
+			int y = find(graph.parent, graph.edges[i].dest);
 
 			if (x == y) {
-				return 1;
+				return true;
 			}
 
-			graph.union(parent, x, y);
+			union(graph.parent, x, y);
 		}
 
-		return 0;
+		return false;
 	}
+
+	int find2(int[] parent, int i) {
+		if (parent[i] == -1) {
+			return i;
+		}
+
+		int res = find2(parent, parent[i]);
+		parent[i] = res;
+		return res;
+	}
+
+	void union2(int[] rank, int[] parent, int x, int y) {
+		int xset = find2(parent, x);
+		int yset = find2(parent, y);
+
+		if (xset == yset) {
+			return;
+		}
+
+		int xrank = rank[xset];
+		int yrank = rank[yset];
+
+		if (xrank < yrank) {
+			parent[xset] = yset;
+		} else if (xrank > yrank) {
+			parent[yset] = xset;
+		} else {
+			parent[xset] = yset;
+			rank[yset]++;
+		}
+	}
+
+	boolean isCircle2(Graph graph) {
+		for (int i=0; i<v; i++) {
+			int x = find2(graph.parent, graph.edges[i].src);
+			int y = find2(graph.parent, graph.edges[i].dest);
+
+			if (x == y) {
+				return true;
+			}
+
+			union2(graph.rank, graph.parent, x, y);
+		}
+
+		return false;
+	}
+}
+
+class Edge {
+	int src, dest;
 }
